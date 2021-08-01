4de9a636205616546f303302a90ac2a36ead0014
Kevin Tooley <kevin.tooley@snhu.edu>
2021-08-01 16:34:25 -0400

    Create Build UI for Featured View.  Change SwithUI to scrollView.  Hide navigationbarTitle.

--- Recipe_List_App/Views/RecipeListView.swift
+++ Recipe_List_App/Views/RecipeListView.swift
@@ -14,29 +14,43 @@ struct RecipeListView: View {
     var body: some View {
         
         NavigationView {
-            List(model.recipes) { r in
+            VStack(alignment: .leading) {
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top, 40)
+                    .font(.largeTitle)
                 
-                NavigationLink(
-                    destination: RecipeDetailView(recipe:r),
-                    label: {
+                
+                ScrollView {
+
+                    LazyVStack(alignment: .leading) {
                         
-                        // MARK: Row Item
-                        HStack(spacing: 20.0) {
-                            Image(r.image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50.0, height: 50.0)
-                                .clipped()
-                                .cornerRadius(5)
-                            Text(r.name)
+                        ForEach (model.recipes) { r in
+                            
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe:r),
+                                label: {
+                                    
+                                    // MARK: Row Item
+                                    HStack(spacing: 20.0) {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50.0, height: 50.0)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                    }
+                                    
+                                })
+
                         }
-                        
-                    })
+                    }
+                }
                 
-                
-                
-            }
-            .navigationBarTitle("All Recipes")
+            }.navigationBarHidden(true)
+            .padding(.leading, 10)
         }
         
     }
@@ -45,5 +59,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
